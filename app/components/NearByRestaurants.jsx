1af88b6ffad7d924c1bd2af311d94a3d4adb4f55
Rahan Bouess <46539830+Absolu242@users.noreply.github.com>
2023-12-16 14:17:55 +0100

    finished the food page

--- app/components/NearByRestaurants.jsx
+++ app/components/NearByRestaurants.jsx
@@ -1,24 +1,28 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import uidata from '../constants/uidata'
-import StoreComponent from './StoreCompon'
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import uidata from "../constants/uidata";
+import StoreComponent from "./StoreCompon";
+import { useNavigation } from "@react-navigation/native";
 
 const NearByRestaurants = () => {
+  const navigation = useNavigation();
+
   return (
-    <View style={{marginLeft: 12}}>
-     <FlatList data={uidata.restaurants}
-     horizontal
-     showsHorizontalScrollIndicator={false}
-     style={{marginTop: 5, rowGap: 10}}
-     scrollEnabled
-     renderItem={({item}) => (
-        <StoreComponent item={item} onPress={()=> {}}/>
-     )}
-     />
+    <View style={{ marginLeft: 12 }}>
+      <FlatList
+        data={uidata.restaurants}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={{ marginTop: 5, rowGap: 10 }}
+        scrollEnabled
+        renderItem={({ item }) => (
+          <StoreComponent item={item} onPress={() => {}} />
+        )}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default NearByRestaurants
+export default NearByRestaurants;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- app/screens/FoodPage.jsx
+++ app/screens/FoodPage.jsx
@@ -5,14 +5,19 @@ import {
   Image,
   TouchableOpacity,
   FlatList,
+  TextInput,
 } from "react-native";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { CartCountContext } from "../context/CartCountContext";
 import { COLORS, SIZES } from "../constants/theme";
 import {
   Ionicons,
   MaterialCommunityIcons,
+  AntDesign,
 } from "@expo/vector-icons";
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import Counter from "../components/Counter";
 
 const FoodPage = ({ route, navigation }) => {
   const item = route.params.item;
@@ -25,6 +30,61 @@ const FoodPage = ({ route, navigation }) => {
   const [preference, setPreference] = useState("");
   //const { cartCount, setCartCount } = useContext(CartCountContext);
 
+  let sendToOrderpage;
+  const id = item.restaurant;
+  const handleAdditives = (newAdditive) => {
+    setAdditives((prevAdditives) => {
+      const exists = prevAdditives.some(
+        (additive) => additive.id === newAdditive.id
+      );
+
+      if (exists) {
+        return prevAdditives.filter(
+          (additive) => additive.id !== newAdditive.id
+        );
+      } else {
+        return [...prevAdditives, newAdditive];
+      }
+    });
+  };
+
+  const handlePress = (item) => {
+    const cartItem = {
+      productId: item._id,
+      additives: additives,
+      quantity: count,
+      totalPrice: (item.price + totalPrice) * count,
+    };
+
+    addToCart(cartItem);
+  };
+
+  sendToOrderpage = {
+    orderItem: {
+      foodId: item._id,
+      additives: additives,
+      quantity: count,
+      price: (item.price + totalPrice) * count,
+      instructions: preference,
+    },
+    title: item.title,
+    description: item.description,
+    imageUrl: item.imageUrl[0],
+    restaurant: id,
+  };
+
+  const addToCart = async (cartItem) => {};
+
+  useEffect(() => {
+    calculatePrice();
+  }, [additives]);
+
+  const calculatePrice = () => {
+    const total = additives.reduce((sum, additive) => {
+      return sum + parseFloat(additive.price);
+    }, 0);
+    setTotalPrice(total);
+  };
   return (
     <View
       style={{
@@ -116,6 +176,144 @@ const FoodPage = ({ route, navigation }) => {
             </View>
           )}
         />
+
+        <Text
+          style={[
+            styles.title,
+            { marginBottom: 10, marginTop: 20 },
+          ]}
+        >
+          Additives and Toppings
+        </Text>
+
+        <FlatList
+          data={item.additives}
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => item.id}
+          style={{ marginTop: 10 }}
+          scrollEnabled={false}
+          renderItem={({ item }) => (
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                marginBottom: 10,
+              }}
+            >
+              <BouncyCheckbox
+                size={20}
+                unfillColor="#FFFFFF"
+                fillColor={COLORS.primary}
+                innerIconStyle={{ borderWidth: 1 }}
+                textStyle={styles.small}
+                text={item.title}
+                onPress={() => {
+                  handleAdditives(item);
+                }}
+              />
+              <Text style={styles.small}>${item.price}</Text>
+            </View>
+          )}
+        />
+
+        <Text
+          style={[
+            styles.title,
+            { marginBottom: 10, marginTop: 20 },
+          ]}
+        >
+          Preferences
+        </Text>
+
+        <View style={styles.input}>
+          <TextInput
+            placeholder="Add specific instructions"
+            value={preference}
+            onChangeText={(value) => setPreference(value)}
+            autoCapitalize="none"
+            autoCorrect="false"
+            styles={{ flex: 1 }}
+          />
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginTop: 20,
+          }}
+        >
+          <Text style={[styles.title, { marginBottom: 10 }]}>
+            Quantity
+          </Text>
+          <Counter count={count} setCount={setCount} />
+        </View>
+      </View>
+
+      <View style={{ left: 8, top: 150 }}>
+        <View style={{ flex: 1, justifyContent: "flex-end" }}>
+          <View style={styles.suspended}>
+            <View style={styles.cart}>
+              <View style={styles.cartRow}>
+                <TouchableOpacity
+                  onPress={() => {}}
+                  style={styles.cartBtn}
+                >
+                  <AntDesign
+                    name="pluscircleo"
+                    size={24}
+                    color={COLORS.lightWhite}
+                  />
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: COLORS.primary,
+                    paddingHorizontal: 80,
+                    borderRadius: 30,
+                  }}
+                  onPress={() =>
+                    navigation.navigate(
+                      "order-page",
+                      sendToOrderpage
+                    )
+                  }
+                >
+                  <Text
+                    style={[
+                      styles.title,
+                      {
+                        color: COLORS.lightWhite,
+                        marginTop: 3,
+                        alignItems: "center",
+                      },
+                    ]}
+                  >
+                    Order
+                  </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  onPress={() => {}}
+                  style={styles.cartBtn}
+                >
+                  <Text
+                    style={[
+                      styles.title,
+                      {
+                        color: COLORS.lightWhite,
+                        marginTop: 3,
+                        alignItems: "center",
+                      },
+                    ]}
+                  >
+                    {count}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -168,5 +366,42 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     backgroundColor: COLORS.primary,
     borderRadius: 8,
+  },
+  input: {
+    borderColor: COLORS.primary1,
+    borderWidth: 1,
+    backgroundColor: COLORS.offwhite,
+    height: 50,
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  suspended: {
+    position: "absolute",
+    zIndex: 999,
+    bottom: 50,
+    width: "100%",
+    alignItems: "center",
+  },
+  cart: {
+    width: SIZES.width - 24,
+    height: 60,
+    justifyContent: "center",
+    backgroundColor: COLORS.primary1,
+    borderRadius: 30,
+  },
+  cartRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 12,
+  },
+  cartBtn: {
+    width: 40,
+    height: 40,
+    borderRadius: 99,
+    justifyContent: "center",
+    backgroundColor: COLORS.primary,
+    alignItems: "center",
   },
 });
